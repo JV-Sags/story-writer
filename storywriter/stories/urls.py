aa1dfa83c0a861bbd9746eda32aed7364ddc3074
JV-Sags <jpsahagun@student.hau.edu.ph>
2025-03-14 20:22:16 +0800

    Update urls.py

--- storywriter/stories/urls.py
+++ storywriter/stories/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StoryViewSet
+from .views import StoryViewSet, CharacterViewSet, StorySettingsViewSet
 
 router = DefaultRouter()
-router.register(r'stories', StoryViewSet)
+router.register(r'stories', StoryViewSet, basename="stories")
+router.register(r'stories/(?P<story_id>\d+)/characters', CharacterViewSet, basename="story-characters")
+router.register(r'stories/(?P<story_id>\d+)/settings', StorySettingsViewSet, basename="story-settings")
+router.register(r'characters', CharacterViewSet, basename="characters")
 
 urlpatterns = [
     path('', include(router.urls)),

da53bf622d8bfe97dce15014912aeae2c4fafab5
JV-Sags <jpsahagun@student.hau.edu.ph>
2025-03-20 19:36:29 +0800

    Update serializers.py

--- storywriter/stories/serializers.py
+++ storywriter/stories/serializers.py
@@ -1,32 +1,28 @@
 from rest_framework import serializers
-from .models import Story, Character, StorySettings, StorySetting
-from .models import Item
+from .models import Story, Character, StorySettings, StorySetting, Item
 
-# Serializer for the Story model
 class StorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Story
         fields = '__all__'
 
-# Serializer for the Character model
 class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = '__all__'
 
-# Serializer for the StorySettings model
 class StorySettingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = StorySettings
         fields = '__all__'
 
-# Serializer for the StorySetting model (Locations)
 class StorySettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = StorySetting
         fields = '__all__'
-        
+
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+##Changed

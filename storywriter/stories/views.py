527d91b7916caf8e92a735f9cc5827a58f437426
JV-Sags <jpsahagun@student.hau.edu.ph>
2025-03-21 20:07:40 +0800

    Update views.py

--- storywriter/stories/views.py
+++ storywriter/stories/views.py
@@ -1,25 +1,29 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from stories.models import Story, Character, StorySettings, StorySetting, Item
 from stories.serializers import StorySerializer, CharacterSerializer, StorySettingsSerializer, StorySettingSerializer, ItemSerializer
 
 class StoryViewSet(viewsets.ModelViewSet):
     queryset = Story.objects.all().order_by('-created_at')
     serializer_class = StorySerializer
+    permission_classes = [IsAuthenticated]  # Requires authentication
 
 class CharacterViewSet(viewsets.ModelViewSet):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
+    permission_classes = [IsAuthenticated]
 
 class StorySettingsViewSet(viewsets.ModelViewSet):
     queryset = StorySettings.objects.all()
     serializer_class = StorySettingsSerializer
+    permission_classes = [IsAuthenticated]
 
 class StorySettingViewSet(viewsets.ModelViewSet):
     queryset = StorySetting.objects.all()
     serializer_class = StorySettingSerializer
+    permission_classes = [IsAuthenticated]
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticated]

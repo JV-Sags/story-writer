a43298840f5c3e39f8f557afe535c6556dd9c608
JV-Sags <jpsahagun@student.hau.edu.ph>
2025-03-21 20:08:13 +0800

    Update urls.py

--- storywriter/storywriter/urls.py
+++ storywriter/storywriter/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('stories.urls')),  # Keeping 'stories.urls'
+    path('api/api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
